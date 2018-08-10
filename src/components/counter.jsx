eee01ede241f5a857380dcdbb75704bcdf2de872
Martin Alejandro Liut <martin.liut@globant.com>
2018-08-10 15:09:00 -0300

    Adding click event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,42 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: []
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
+    stylesSpan = {
+        fontWeight: 'bold',
+        fontSize: 12,
+        padding: "10px 5px"
+    }
+
     render() { 
+        let spanClasses = this.getBadgeClasses();
+
         return (
             <div>
-                { this.state.tags.length === 0 && 'No tags' }
+                <span style={this.stylesSpan} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-default btn-sm">Increment</button>
+                <ul>
+                    { this.state.tags.map(tag => <li key={tag  }>{tag}</li>)}
+                </ul>
             </div>
         );
+    }
+    
+    handleIncrement() {
+        console.log('Increment clicked');
+    }
+
+    getBadgeClasses() {
+        let spanClasses = "badge m-2 badge-";
+        spanClasses += this.state.count === 0 ? 'warning' : 'primary';
+        return spanClasses;
+    }
+
+    formatCount() {
+        const {count} = this.state ; 
+        return count === 0 ? 'Zero' : count; 
     }
 }
  
