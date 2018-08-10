9fd545292169243879c08c3e694475ee1e389cf1
Martin Alejandro Liut <martin.liut@globant.com>
2018-08-10 14:54:41 -0300

    First way to do a condition

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,38 +3,21 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: []
     };
 
-    stylesSpan = {
-        fontWeight: 'bold',
-        fontSize: 12,
-        padding: "10px 5px"
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>'No tags'</p>;
+
+        return <ul>{ this.state.tags.map(tag => <li key={tag  }>{tag}</li>) }</ul>;
     }
 
     render() { 
-        let spanClasses = this.getBadgeClasses();
-
         return (
             <div>
-                <span style={this.stylesSpan} className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-default btn-sm">Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key={tag  }>{tag}</li>)}
-                </ul>
+                { this.renderTags() }
             </div>
         );
-    }
-     
-    getBadgeClasses() {
-        let spanClasses = "badge m-2 badge-";
-        spanClasses += this.state.count === 0 ? 'warning' : 'primary';
-        return spanClasses;
-    }
-
-    formatCount() {
-        const {count} = this.state ; 
-        return count === 0 ? 'Zero' : count; 
     }
 }
  
